fb43b3606ac4bb13ccdeff33129dbf86d5fa5dc3
Danny12244 <pradeep.py725@gmail.com>
2022-11-03 19:28:27 +0530

    changed url

--- quiz-system/Backend/config/db.js
+++ quiz-system/Backend/config/db.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 
 require('dotenv').config();
 
-const connection = mongoose.connect("mongodb+srv://pradeepD:pradeep123@cluster0.r7tllok.mongodb.net/quizz?retryWrites=true&w=majority");
+const connection = mongoose.connect(process.env.MONGO_URL);
 
 module.exports = connection;
